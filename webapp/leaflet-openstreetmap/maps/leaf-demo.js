cd47055b1c61d822afd42e45d402a943f7dac131
Jerry Chong <jerrychong25@gmail.com>
2022-01-24 17:01:13 +0800

    Recommit web app folder

---
+++ webapp/leaflet-openstreetmap/maps/leaf-demo.js
@@ -0,0 +1,49 @@
+var map = L.map("map", {
+  center: [3.868114, 102.201689],
+  minZoom: 2,
+  zoom: 5.5,
+});
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  subdomains: ["a", "b", "c"],
+}).addTo(map);
+
+L.geoJSON(kelantan_states).addTo(map);
+
+var myURL = jQuery('script[src$="leaf-demo.js"]')
+  .attr("src")
+  .replace("leaf-demo.js", "");
+
+var myIcon = L.icon({
+  iconUrl: myURL + "images/pin24.png",
+  iconRetinaUrl: myURL + "images/pin48.png",
+  iconSize: [29, 24],
+  iconAnchor: [9, 21],
+  popupAnchor: [0, -14],
+});
+
+for (var i = 0; i < markers.length; ++i) {
+  L.marker([markers[i].lat, markers[i].lng], { icon: myIcon })
+    .bindPopup(
+      '<a href="' +
+        markers[i].url +
+        '" target="_blank">' +
+        markers[i].name +
+        "</a>"
+    )
+    .addTo(map);
+}
+
+// for (var i = 0; i < kelantan_states.length; ++i) {
+//   L.marker([kelantan_states[i].lat, kelantan_states[i].lng])
+//     .bindPopup(
+//       '<a href="' +
+//         kelantan_states[i].url +
+//         '" target="_blank">' +
+//         kelantan_states[i].name +
+//         "</a>"
+//     )
+//     .addTo(map);
+// }
---
+++ webapp/leaflet-openstreetmap/maps/markers.js
@@ -0,0 +1,26 @@
+var markers = [
+  {
+    name: "Terengganu",
+    url: "https://en.wikipedia.org/wiki/Terengganu",
+    lat: 4.934245,
+    lng: 103.003832,
+  },
+  {
+    name: "Kelantan",
+    url: "https://en.wikipedia.org/wiki/Kelantan",
+    lat: 5.566706,
+    lng: 102.020242,
+  },
+  {
+    name: "Pahang",
+    url: "https://en.wikipedia.org/wiki/Pahang",
+    lat: 3.413045,
+    lng: 103.005981,
+  },
+  {
+    name: "Johor",
+    url: "https://en.wikipedia.org/wiki/Johor",
+    lat: 2.015493,
+    lng: 103.571209,
+  },
+];
