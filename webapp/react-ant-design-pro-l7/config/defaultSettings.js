f7fabd6b2d791a732f0edfb98afce35275ecbed9
Jerry Chong <jerrychong25@gmail.com>
2022-01-27 12:52:11 +0800

    Modified Web App React Title

--- webapp/react-ant-design-pro-l7/config/defaultSettings.js
+++ webapp/react-ant-design-pro-l7/config/defaultSettings.js
@@ -7,7 +7,7 @@ const Settings = {
   fixedHeader: false,
   fixSiderbar: true,
   colorWeak: false,
-  title: 'Ant Design Pro',
+  title: 'Soteria',
   pwa: false,
   logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
   iconfontUrl: '',
